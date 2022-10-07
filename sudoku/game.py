7fc306040c93ab5e634350d6534be76d5e8c51cd
Euler0525 <13804800525@139.com>
2022-10-07 23:06:26 +0800

    remove the value which has been confirmed

--- sudoku/game.py
+++ sudoku/game.py
@@ -1,4 +1,6 @@
 import pprint
+from copy import deepcopy
+
 
 class Grid(object):
     def __init__(self, x: int, y: int):
@@ -7,15 +9,33 @@ class Grid(object):
 
 
 def initField(origin: list) -> list:
-    init = []
+    init = deepcopy(origin)
     for i in range(9):
         for j in range(9):
             if len(origin[i][j]) == 1:
-                init.append([origin[i][j][0]])
+                init[i][j] = [origin[i][j][0]]
             else:
-                init.append([1, 2, 3, 4, 5, 6, 7, 8, 9])
+                init[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
     pprint.pprint(init)
     return init
+
+
+def removeValue(origin: list) -> list:
+    result = deepcopy(origin)
+    for i in range(9):
+        for j in range(9):
+            for k in range(9):
+                if len(result[i][k]) == 1 and result[i][k][0] in result[i][j]:
+                    result[i][j].remove(result[i][k][0])
+                if len(result[k][j]) == 1 and result[k][j][0] in result[i][j]:
+                    result[i][j].remove(result[k][j][0])
+            for m in range(i // 3 * 3, i // 3 * 3 + 3):
+                for n in range(j // 3 * 3, j // 3 * 3 + 3):
+                    if len(result[m][n]) == 1 and result[m][n][0] in result[i][j]:
+                        result[i][j].remove(result[m][n][0])
+    pprint.pprint(result)
+    return result
 
 
 if __name__ == "__main__":
@@ -28,4 +48,5 @@ if __name__ == "__main__":
             [[], [], [1], [], [], [], [], [6], [8]],
             [[], [], [8], [5], [], [], [], [1], []],
             [[], [9], [], [], [], [], [4], [], []]]
-    initField(test)
+
+    removeValue(initField(test))
